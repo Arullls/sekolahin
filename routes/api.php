fccc8ed60cff2428b9ed0c79f6aca03da7c5ecea
Arullls <sahrulharmain12@gmail.com>
2025-06-13 17:33:38 +0700

    Add DashboardController and define resource methods

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
 // routes/api.php
 Route::middleware('auth:sanctum')->get('/dashboard', [DashboardController::class, 'index']);
